20cb4d85330c1c71c83f6b0f599f4262e64be111
Maeyanie <me@maeyanie.com>
2021-11-10 22:34:37 -0500

    Some refactoring and quieting.

--- Makefile
+++ Makefile
@@ -9,20 +9,9 @@ clean:
 crc32c.o: crc32c.c
 	$(CC) $(CFLAGS) -c -o $@ $^
 
-stl2sml.o: stl2sml.cpp
-	$(CXX) $(CXXFLAGS) -c -o $@ $^
-mesh.o: mesh.cpp
-	$(CXX) $(CXXFLAGS) -c -o $@ $^
-stl.o: stl.cpp
-	$(CXX) $(CXXFLAGS) -c -o $@ $^
-sml.o: sml.cpp
-	$(CXX) $(CXXFLAGS) -c -o $@ $^
-obj.o: obj.cpp
-	$(CXX) $(CXXFLAGS) -c -o $@ $^
-
-stl2sml: stl2sml.o crc32c.o mesh.o stl.o sml.o
+stl2sml: stl2sml.o crc32c.o mesh.o stl.o sml.o stripsearch.o
 	$(CXX) $(CXXFLAGS) -o $@ $^
-sml2stl: sml2stl.o crc32c.o mesh.o stl.o sml.o
+sml2stl: sml2stl.o crc32c.o mesh.o stl.o sml.o stripsearch.o
 	$(CXX) $(CXXFLAGS) -o $@ $^
-obj2sml: obj2sml.o crc32c.o mesh.o obj.o sml.o
+obj2sml: obj2sml.o crc32c.o mesh.o obj.o sml.o stripsearch.o
 	$(CXX) $(CXXFLAGS) -o $@ $^
--- sml.h
+++ sml.h
@@ -2,6 +2,7 @@
 #define SML_H
 
 #include <filesystem>
+#include "mesh.h"
 
 extern "C" {
 	uint32_t crc32c_sw(uint32_t crc, void const *buf, size_t len);
@@ -13,10 +14,15 @@ enum SMLFlags {
 	NONE				= 0,
 	STRIP_NEXT			= 0b0001,
 	STRIP_MAP			= 0b0010,
-	STRIP_EXHAUSTIVE	= 0b0100
+	STRIP_EXHAUSTIVE	= 0b0100,
+	STRIP               = 0b0111
 };
 
 Mesh* readSML(std::filesystem::path file);
 void writeSML(std::filesystem::path file, Mesh* mesh, uint32_t writeFlags = SMLFlags::NONE);
+
+void stripsearch_map(Mesh* mesh, std::list<Triangle*>& singles, std::list<std::list<Triangle*>>& strips);
+void stripsearch_next(Mesh* mesh, std::list<Triangle*>& singles, std::list<std::list<Triangle*>>& strips);
+void stripsearch_exhaustive(Mesh* mesh, std::list<Triangle*>& singles, std::list<std::list<Triangle*>>& strips);
 
 #endif
